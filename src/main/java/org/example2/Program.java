379c806310aca6fcd10d4ddbc8e57cbda5100dd0
oleg <legik7127@gmail.com>
2023-06-05 16:34:00 +0600

    created variables

--- src/main/java/org/example2/Program.java
+++ src/main/java/org/example2/Program.java
@@ -9,12 +9,24 @@ public class Program {
     private static final char DOT_EMPTY = '.';
 
     private static final Scanner scanner = new Scanner(System.in);
+    private static char[][] field;
     private static final Random random = new Random();
-
+    private static int fieldSizeX;
+    private static int fieldSizeY;
 
 
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+    }
+    private static void initialize(){
+        fieldSizeX = 3;
+        fieldSizeY = 3;
+
+        field = new char[fieldSizeX][fieldSizeY];
+        for(int y =0; y < fieldSizeY; y++){
+            for(int x = 0; x< fieldSizeX; x++){
+                field[x][y] = DOT_EMPTY;
+            }
+        }
     }
 }
