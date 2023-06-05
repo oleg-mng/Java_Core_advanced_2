6e23bc240c1351b20697e03050d96441d52739e5
oleg <legik7127@gmail.com>
2023-06-05 18:03:08 +0600

    created methods initialize() and printField()

--- src/main/java/org/example2/Program.java
+++ src/main/java/org/example2/Program.java
@@ -16,17 +16,41 @@ public class Program {
 
 
     public static void main(String[] args) {
+        initialize();
+        printField();
 
     }
-    private static void initialize(){
-        fieldSizeX = 3;
-        fieldSizeY = 3;
+
+    private static void initialize() {
+        fieldSizeX = 10;
+        fieldSizeY = 5;
 
         field = new char[fieldSizeX][fieldSizeY];
-        for(int y =0; y < fieldSizeY; y++){
-            for(int x = 0; x< fieldSizeX; x++){
-                field[x][y] = DOT_EMPTY;
+        for (int x = 0; x < fieldSizeY; x++) {
+            for (int y = 0; y < fieldSizeX; y++) {
+                field[y][x] = DOT_EMPTY;
             }
         }
+    }
+
+    private static void printField() {
+        System.out.print("+");
+        for (int i = 0; i < fieldSizeX * 2 + 1; i++) {
+            System.out.print(i % 2 == 0 ? "-" : i / 2 + 1);
+        }
+        System.out.println();
+
+        for(int i = 0; i <fieldSizeY; i++){
+            System.out.print(i+1 +"|");
+            for (int j = 0; j < fieldSizeX; j++){
+                System.out.print(field[j][i]+"|");
+            }
+            System.out.println();
+        }
+
+        for (int i = 0; i < fieldSizeX * 2 + 2; i++) {
+            System.out.print("-");
+        }
+
     }
 }
