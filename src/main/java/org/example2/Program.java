e5a6f60de35d39435d89cd6a801c46a0ad241537
oleg <legik7127@gmail.com>
2023-06-05 16:14:08 +0600

    Initial

---
+++ src/main/java/org/example2/Program.java
@@ -0,0 +1,20 @@
+package org.example2;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Program {
+    private static final char DOT_HUMAN = 'X';
+    private static final char DOT_AI = 'Y';
+    private static final char DOT_EMPTY = '.';
+
+    private static final Scanner scanner = new Scanner(System.in);
+    private static final Random random = new Random();
+
+
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+    }
+}
